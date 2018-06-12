af53a87dfd2031654e6082bbbaa699b4585da257
cnvp21 <cnvp21@gmail.com>
2018-06-12 16:31:49 +0800

    README

--- README.md
+++ README.md
@@ -8,13 +8,66 @@ API 来自[https://github.com/Binaryify/NeteaseCloudMusicApi]
 
 Vue + Vue Router + Vue CLI + Vuetify
 
+
+
+## 快速使用
+
 ```bash
+# 克隆项目到本地
 git clone https://github.com/cnvp21/vue-cloud-music.git
 
+# 定位到项目目录
 cd vue-cloud-music
 
+# 安装依赖
 npm install
 
+# 启动项目
 npm run serve
 ```
+
+
+
+## 技术
+
+- [Vue](https://vuejs.org/)
+
+  使用了Vue作为项目的框架
+
+- [Vue CLI](https://cli.vuejs.org/)
+
+  使用Vue.js官方的脚手架工具搭建开发环境
+
+- [Vue Router](https://router.vuejs.org/)
+
+  Vue.js 官方路由管理器
+
+- [Vuetify](https://vuetifyjs.com/)
+
+  Material Design 的UI 组件
+
+
+
+### 功能
+
+实现了[网易云音乐](http://music.163.com/)移动端的功能，包括：
+
+- 推荐、榜单、搜索三个主要页面
+- 歌单页面
+- 歌曲播放页面
+- 搜索功能
+  - 热搜关键词
+  - 搜索输入提示
+  - 搜索结果展示
+- 歌单页面
+
+
+
+待实现功能：
+
+- 歌曲以及歌单评价展示
+
+- 歌单播放次数
+
+  ……
 
--- src/components/Song.vue
+++ src/components/Song.vue
@@ -2,29 +2,38 @@
     <div>
         <v-layout>
             <v-flex xs12 sm12>
-                <v-card> 
+                <v-card>
                     <v-card-media :src="song.pic" height="400px">
                     </v-card-media>
                     <v-card-title primary-title>
-                        <h1>{{song.title}}</h1>                        
-                    </v-card-title>                    
+                        <h1>{{song.title}}</h1>
+                    </v-card-title>
                 </v-card>
-                <audio :src="song.url" controls ></audio>
+                <audio id="player" :src="song.url" controls></audio>
+                <v-flex xs12>
+                    <!-- custome play control to do  -->
+                </v-flex>
             </v-flex>
         </v-layout>
     </div>
 </template>
 <script>
+document.getElementById("myVideo");
+
 import axios from "axios";
 
 export default {
   data: function() {
     return {
       song: {
-          title: '',
-          pic: '',
-          url: 'http://music.163.com/song/media/outer/url?id='+this.$route.query.id+'.mp3 '
-      }
+        title: "",
+        pic: "",
+        url:
+          "http://music.163.com/song/media/outer/url?id=" +
+          this.$route.query.id +
+          ".mp3 "
+      },
+      isPlaying: false
     };
   },
   methods: {},
@@ -33,8 +42,11 @@ export default {
       .get("http://47.106.119.139:3000/song/detail?ids=" + this.$route.query.id)
       .then(response => {
         this.song.title = response.data.songs[0].name;
-        this.song.pic = response.data.songs[0].al.picUrl;        
+        this.song.pic = response.data.songs[0].al.picUrl;
       });
+  },
+  methods: {
+    
   }
 };
 </script>
