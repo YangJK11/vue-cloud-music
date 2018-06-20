20e9183d4edcbf924662f417fbbf58644f761d16
cnvp21 <cnvp21@gmail.com>
2018-06-20 18:20:41 +0800

    修改toolbar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
       <v-toolbar color="red" dark tabs fixed>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
         <v-toolbar-title>Cloud Music</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-btn icon>
-          <v-icon>more_vert</v-icon>
+          <!-- <v-icon>more_vert</v-icon> -->
         </v-btn>
         <v-tabs slot="extension" centered color="red" slider-color="white">
           <v-tab to="/recommended" replace>推荐音乐</v-tab>
